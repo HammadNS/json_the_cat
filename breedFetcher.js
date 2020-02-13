dad6afd908164e6b6654b35e24e8714dcd2c3422
hammadNS96 <hammad96@outlook.com>
2020-02-13 18:29:31 +0000

    cat restructure

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,25 @@
 
 const request = require('request');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, resp, body) => {
-  if (error) {
-    return console.log('Failed to find details about cat ', error);
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
-});
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 
+
+const fetchBreedDescription = (breedName, callback) => {
+  request(url + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+if (!error) {
+if (data.length === 0) {
+  callback(null, `${breedName} not found`);
+} else {
+      
+      }
+  }
+   else {
+      callback(error);
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
